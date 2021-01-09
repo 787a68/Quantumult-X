5103104ce7f4aa6f3694c6e6c19dbcf2795cc9be
787a68 <36852208+787a68@users.noreply.github.com>
2021-01-09 20:35:17 +0800

    up

--- Script/Lookup.js
+++ Script/Lookup.js
@@ -17,22 +17,24 @@ if (body.status == "success") {
   var city = "City: " + body.city + "\n\n";
   var region = "Region: " + body.regionName + "\n\n";
   var country = "Country: " + body.country + "\n\n";
-
+  
+  //校验国家、地区、城市是否相同
   var location =
-    body.country == body.regionName
-      ? body.regionName == body.city
-        ? city
-        : city + region
-      : body.country == body.city
-        ? region + city
-        : body.city == body.regionName
-          ? city + country
-          : city + region + country;
+    body.country == body.regionName //比较国家与地区
+      ? body.regionName == body.city //国家=地区，比较地区与城市
+        ? city //国家=地区=城市，只显示城市
+        : city + region //国家=地区!=城市，显示城市+地区
+            
+      : body.country == body.city //国家!=地区，比较国家与城市
+        ? region + city //国家!=地区，但国家=城市，显示地区+城市
+        : body.city == body.regionName //国家!=地区，国家!=城市，比较城市与地区
+          ? city + country //国家!=地区，国家!=城市，但城市=地区，显示城市+国家
+          : city + region + country; //都不相同全显示
 
   var org =
-    body.isp == body.org
-      ? "ORG: " + body.org
-      : "ISP: " + body.isp + "\n\n" + "ORG: " + body.org;
+    body.isp == body.org //校验组织（公司）与运营商
+      ? "ORG: " + body.org //相同只显示组织
+      : "ISP: " + body.isp + "\n\n" + "ORG: " + body.org; //不相同全显示
 
   var description = (
     "\n" +
@@ -41,8 +43,8 @@ if (body.status == "success") {
     "IP: " + body.query + "\n\n" +
     org + "\n\n" +
     "BGP: " + body.as)
-    .replace(/\n\w+?:\s(?:\n|$)/g, "")
-    .replace(/\n$/, "");
+    .replace(/\n\w+?:\s(?:\n|$)/g, "") //去除没有内容的行
+    .replace(/\n$/, ""); //去除最后的换行符号（如果有）
 
   $done({ title, subtitle, ip, description });
 } else $done();
