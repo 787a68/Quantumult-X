04da1d93aa4f176a99e93a3e01fee3b1b59b1734
787a68 <36852208+787a68@users.noreply.github.com>
2023-01-07 23:39:54 +0800

    up

--- Scripts/wps.js
+++ Scripts/wps.js
@@ -3,48 +3,22 @@
 ^https?+:\/\/sg-account\.wps\.com:443\/api\/users\/url script-response-body https://raw.githubusercontent.com/787a68/Quantumult-X/main/Scripts/wps.js
 
 [mitm]
-hostname = account.wps.com, account.wps.cn
-
+hostname = sg-account.wps.com
 */
 
-var body = JSON.parse($response.body);
 
-body = {
+var body = {
   result: "ok",
   total_cost: 0,
   privilege: [
-    {
-      times: 0,
-      expire_time: 1893427200,
-      spid: "data_recover"
-    },
-    {
-      times: 0,
-      expire_time: 1893427200,
-      spid: "ocr"
-    },
-    {
-      times: 0,
-      expire_time: 1893427200,
-      spid: "pdf2doc"
-    },
-    {
-      times: 0,
-      expire_time: 1893427200,
-      spid: "pdf_merge"
-    },
-    {
-      times: 0,
-      expire_time: 1893427200,
-      spid: "pdf_sign"
-    },
-    {
-      times: 0,
-      expire_time: 1893427200,
-      spid: "pdf_split"
-    }
+    { times: 0, expire_time: 1893427200, spid: "data_recover" },
+    { times: 0, expire_time: 1893427200, spid: "ocr" },
+    { times: 0, expire_time: 1893427200, spid: "pdf2doc" },
+    { times: 0, expire_time: 1893427200, spid: "pdf_merge" },
+    { times: 0, expire_time: 1893427200, spid: "pdf_sign" },
+    { times: 0, expire_time: 1893427200, spid: "pdf_split" }
   ],
-  userid: body.userid,
+  userid: JSON.parse($response.body).userid,
   wealth: 0,
   level: 1,
   exp: 0,
@@ -54,21 +28,9 @@ body = {
     name: "超级会员",
     has_ad: 0,
     enabled: [
-      {
-        name: "稻壳会员",
-        expire_time: 1893427200,
-        memberid: 12
-      },
-      {
-        name: "超级会员",
-        expire_time: 1893427200,
-        memberid: 40
-      },
-      {
-        name: "WPS会员",
-        expire_time: 1893427200,
-        memberid: 20
-      }
+      { name: "稻壳会员", expire_time: 1893427200, memberid: 12 },
+      { name: "超级会员", expire_time: 1893427200, memberid: 40 },
+      { name: "WPS会员", expire_time: 1893427200, memberid: 20 }
     ]
   },
   total_buy: 0
